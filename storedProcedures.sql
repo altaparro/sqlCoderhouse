8e1beb546731f4eebeb5c049896b27b99b0c942f
altaparro <101281977+altaparro@users.noreply.github.com>
2024-04-26 13:05:42 -0300

    Update storedProcedures.sql

--- storedProcedures.sql
+++ storedProcedures.sql
@@ -1,16 +1,36 @@
 use sistema_ventas;
 
+-- PRIMER STOREDPROCEDURE.
+
 DELIMITER $$
-CREATE PROCEDURE ordenar_por_id(IN tabla_nombre VARCHAR(255), IN orden VARCHAR(4))
+CREATE PROCEDURE ordenar_por_id(IN proveedores VARCHAR(255), IN orden VARCHAR(4))
 BEGIN
-    SET @query = CONCAT('SELECT * FROM ', tabla_nombre, ' ORDER BY id ', orden);
+    DECLARE query_string VARCHAR(1000);
+    SET @query = CONCAT('SELECT * FROM ', proveedores, ' ORDER BY id ', orden);
     PREPARE stmt FROM @query;
     EXECUTE stmt;
     DEALLOCATE PREPARE stmt;
 END$$
 DELIMITER ;
 
+-- llamado a dicho stored procedure
+
+CALL ordenar_por_id('proveedores', 'ASC');
+
+-- SEGUNDO STOREDPROCEDURE
+
+DELIMITER $$
+CREATE PROCEDURE ordenar_por_columna(IN tabla_nombre VARCHAR(255), IN columna_nombre VARCHAR(255), IN orden VARCHAR(4))
+BEGIN
+    DECLARE query_string VARCHAR(1000);
+    SET @query = CONCAT('SELECT * FROM ', tabla_nombre, ' ORDER BY ', columna_nombre, ' ', orden);
+    PREPARE stmt FROM @query;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
+END$$
+DELIMITER ;
 
 -- llamado a dicho stored procedure
 
-CALL ordenar_por_id('nombre_de_tabla', 'ASC');
+CALL ordenar_por_columna('Ventas', 'fecha', 'ASC');
+
